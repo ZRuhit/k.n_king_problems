7f18b422bc4d8bab5bfa928097e5e005d7b2230e
ZRuhit <113055001+ZRuhit@users.noreply.github.com>
2022-09-15 00:07:27 +0600

    Create 4.3.c

---
+++ 4.3.c
@@ -0,0 +1,15 @@
+/revalsal of a three digit number without using arithmetic to split the number into digits
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    scanf("%1d%1d%1d",&a,&b,&c);
+    printf("%d%d%d",c,b,a);
+
+    return 0;
+
+}
