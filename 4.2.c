3b1d5c6218c0a919ec6361fbaa4f0120de8515cd
ZRuhit <113055001+ZRuhit@users.noreply.github.com>
2022-09-14 22:22:23 +0600

    Update 4.2.c

--- 4.2.c
+++ 4.2.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,X,Y,Z,P;
+    scanf("%d",&a);
+
+    X=a/100;
+    Y=a%100;
+    Z=Y/10;
+    P=Y%10;
+
+    printf("%d%d%d",P,Z,X);
+
+    return 0;
+}
 
